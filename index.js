37478a4c4fdf6553dcfb7577b7616afc0cbcd050
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2020-10-14 15:58:47 -0400

    relevance script

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const client = algoliasearch('APP_ID','ADMIN_API_KEY');
 
 const index = client.initIndex('demo_ecommerce');
 
-fetch('https://alg.li/doc-ecommerce.json')
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(products) {
-    return index.saveObjects(products, {
-      autoGenerateObjectIDIfNotExist: true
-    })
-  })
+// Step 1: indexing script
+// fetch('https://alg.li/doc-ecommerce.json')
+//   .then(function(response) {
+//     return response.json()
+//   })
+//   .then(function(products) {
+//     return index.saveObjects(products, {
+//       autoGenerateObjectIDIfNotExist: true
+//     })
+//   })
+
+// Step 2: relevance script
+index.setSettings({
+    // select attributes for search
+    searchableAttributes: ['brand', 'name', 'categories', 'description'],
+    // business metrics for custom ranking
+    customRanking: ['desc(popularity)'] ,
+    // select attributes for filtering
+    attributesForFaceting: ['categories', 'searchable(brand)', 'price']
+})
